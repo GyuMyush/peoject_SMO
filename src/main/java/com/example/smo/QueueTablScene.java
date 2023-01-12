b8f24be49061af64b24e93b3659f5eb9e400fa1e
artem <gyumyush228@gmail.com>
2023-01-12 19:29:58 +0300

    новое окно вывода очереди (окно > номер в очереди)

---
+++ src/main/java/com/example/smo/QueueTablScene.java
@@ -0,0 +1,23 @@
+package com.example.smo;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
+
+public class QueueTablScene {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private ListView<String> queueList;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
